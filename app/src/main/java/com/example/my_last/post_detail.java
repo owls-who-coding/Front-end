aa3193f3941cea670ed647968c6aff0779b6fbe3
윤상일 <yun090405>
2023-04-08 15:41:00 +0900

    feat:add activity(post_detail) and class(post_detail) change MainActivity to Fragment

---
+++ app/src/main/java/com/example/my_last/post_detail.java
@@ -0,0 +1,40 @@
+package com.example.my_last;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class user_write extends Fragment {
+
+    Button back;
+    community community;
+
+    @SuppressLint("MissingInflatedId")
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+        View view = inflater.inflate(R.layout.activity_diary, null);
+
+        back = (Button) view.findViewById(R.id.back_community);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getParentFragmentManager().beginTransaction().replace(R.id.containers, community).commit();
+            }
+        });
+
+
+        //return inflater.inflate(R.layout.activity_community,container,false);
+        return view;
+    }
+}
