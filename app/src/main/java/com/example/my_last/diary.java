a2641b7289249145b948108aedc2433ca68e628d
fheodaos <yun090405@gmail.com>
2023-04-20 21:32:43 +0900

    feat: diary_chart

--- app/src/main/java/com/example/my_last/diary.java
+++ app/src/main/java/com/example/my_last/diary.java
@@ -2,35 +2,37 @@ package com.example.my_last;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
-import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.text.LineBreakConfig;
 import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.EdgeEffect;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 
 
@@ -43,6 +45,13 @@ public class diary extends Fragment implements CalendarView.OnDateChangeListener
 
     TextView y_kg,y_eat,y_count,y_out;
 
+    LineChart lineChart;
+
+    public int selectedYear;
+    public int selectedMonth;
+    public int selectedDay;
+
+    @SuppressLint("MissingInflatedId")
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
 
 
@@ -56,14 +65,121 @@ public class diary extends Fragment implements CalendarView.OnDateChangeListener
 
         sharedPreferences = context.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
 
+        lineChart = (LineChart) view.findViewById(R.id.chart);
+
+        setupLineChart();
+
+
+
         return view;
 
     }
+
+    private void setupLineChart() {
+        // 라인 차트의 설명 설정
+        Description description = new Description();
+        description.setText("지난 7일");
+        description.setTextColor(Color.BLACK);
+        lineChart.setDescription(description);
+
+        // 라인 차트의 레전드 설정
+        lineChart.getLegend().setEnabled(false);
+
+        // 라인 차트의 X축 설정
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextColor(Color.BLACK);
+
+        // 라인 차트의 Y축 설정
+        lineChart.getAxisRight().setEnabled(false);
+        lineChart.getAxisLeft().setTextColor(Color.BLACK);
+
+        // 라인 차트의 배경색 설정
+        lineChart.setBackgroundColor(Color.WHITE);
+
+        // 라인 차트의 애니메이션 설정
+        lineChart.animateXY(1000, 1000);
+    }
+
+    private void updateLineChart() {
+
+        Calendar startDate = Calendar.getInstance();
+        startDate.set(selectedYear,selectedMonth,selectedDay);
+        startDate.add(Calendar.DAY_OF_MONTH,-7);
+
+//        String s = Integer.toString(startDate.get(Calendar.YEAR)) + Integer.toString(startDate.get(Calendar.MONTH)) + Integer.toString(startDate.get(Calendar.DAY_OF_MONTH)) ;
+//        Toast.makeText(context, s, Toast.LENGTH_SHORT).show();
+
+        Calendar endDate = Calendar.getInstance();
+        endDate.set(selectedYear,selectedMonth,selectedDay);
+
+        // 라인 차트에 표시할 데이터 리스트 생성
+        List<Entry> entries = new ArrayList<>();
+
+        Calendar currentDate = (Calendar) startDate.clone();
+
+        int count = 0;
+
+        while(currentDate.compareTo(endDate) <= 0){
+
+            String str = Integer.toString(currentDate.get(Calendar.MONTH)) + "." + Integer.toString(currentDate.get(Calendar.DAY_OF_MONTH));
+
+
+            // SharedPreferences에서 해당 날짜의 Kg 값을 가져옴
+            float weight = getWeightFromSharedPreferences(currentDate);
+
+
+            Entry entry = new Entry(count++,weight);
+
+            // 그래프 데이터에 날짜와 Kg 값을 추가
+            entries.add(entry);
+
+            currentDate.add(Calendar.DAY_OF_MONTH, 1);
+        }
+
+        // 라인 차트에 데이터 설정
+        LineDataSet dataSet = new LineDataSet(entries, "KG");
+        dataSet.setColor(Color.RED);
+        dataSet.setLineWidth(2f);
+        LineData lineData = new LineData(dataSet);
+        lineChart.setData(lineData);
+        lineChart.invalidate(); // 차트 갱신
+    }
+
+    public float getWeightFromSharedPreferences(Calendar currentDate){
+
+//        String key = year + "/" +"0"+ month + "/" + day;
+//        Float weight = sharedPreferences.getFloat(key, 0.0f);
+
+        String y = Integer.toString(currentDate.get(Calendar.YEAR));
+        String m = Integer.toString(currentDate.get(Calendar.MONTH));
+        String d = Integer.toString(currentDate.get(Calendar.DAY_OF_MONTH));
+
+        String dateTime = y+"/"+"0"+m+"/"+d;
+
+        String kg = sharedPreferences.getString(dateTime + "_kg","");
+
+//        Toast.makeText(context, "("+kg+"!!!)", Toast.LENGTH_SHORT).show();
+
+//        float Kg = Float.parseFloat(kg);
+
+        if(kg == ""){
+            return 0;
+        }else{
+            return Float.parseFloat(kg);
+        }
+    }
+
+
     @Override
     public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
         calendar.set(Calendar.YEAR, year);
         calendar.set(Calendar.MONTH, month);
         calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+        selectedYear = year;
+        selectedMonth = month+1;
+        selectedDay = dayOfMonth;
 
         showInputDialog(year,month+1,dayOfMonth);
     }
@@ -80,7 +196,7 @@ public class diary extends Fragment implements CalendarView.OnDateChangeListener
         final EditText Out = view.findViewById(R.id.out);
         final EditText Problem = view.findViewById(R.id.problem);
 
-        String dataTime = getFormattedDateTime();
+        String dataTime = year+"/"+"0"+month+"/"+dayOfMonth;
 
         String kg = sharedPreferences.getString(dataTime + "_kg","");
         String eat = sharedPreferences.getString(dataTime + "_eat","");
@@ -112,7 +228,9 @@ public class diary extends Fragment implements CalendarView.OnDateChangeListener
                         String count = Count.getText().toString();
                         String out = Out.getText().toString();
                         String problem = Problem.getText().toString();
-                        String dateTime = getFormattedDateTime();
+                        String dateTime = year+"/"+"0"+month+"/"+dayOfMonth;
+
+//                        Toast.makeText(context, dateTime, Toast.LENGTH_SHORT).show();
 
                         SharedPreferences.Editor editor = sharedPreferences.edit();
                         editor.putString(dateTime + "_kg", kg);
@@ -122,7 +240,11 @@ public class diary extends Fragment implements CalendarView.OnDateChangeListener
                         editor.putString(dateTime + "_problem", problem);
                         editor.apply();
 
-                        Toast.makeText(context, "내용이 저장되었습니다. (" + dateTime + ")", Toast.LENGTH_SHORT).show();
+//                        Toast.makeText(context, "내용이 저장되었습니다. (" + dateTime + ")", Toast.LENGTH_SHORT).show();
+//                        Toast.makeText(context, kg+"!", Toast.LENGTH_SHORT).show();
+//                        Toast.makeText(context, kg+"!!", Toast.LENGTH_SHORT).show();
+                        updateLineChart();
+
                     }
                 })
                 .setNegativeButton("취소", new DialogInterface.OnClickListener() {
