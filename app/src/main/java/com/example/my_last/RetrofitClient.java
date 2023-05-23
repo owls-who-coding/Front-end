d4b1c01c9427a7cc13a4b61c755b817f58a0e3ce
unknown <joonhee305@naver.com>
2023-05-23 16:54:11 +0900

    feat:addPredictResult

--- app/src/main/java/com/example/my_last/RetrofitClient.java
+++ app/src/main/java/com/example/my_last/RetrofitClient.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
-    private static final String BASE_URL = "https://7a35-125-133-41-82.jp.ngrok.io";
+    private static final String BASE_URL = "https://245b-222-117-126-33.ngrok-free.app";
 
     private static Retrofit retrofit = null;
 
