adc0ef6a184fd7eb1ed36c927dac4329b8115e4b
unknown <joonhee305@naver.com>
2023-09-10 21:30:24 +0900

    remove properties

--- app/src/main/java/com/example/my_last/RetrofitClient.java
+++ app/src/main/java/com/example/my_last/RetrofitClient.java
@@ -1,16 +1,19 @@
 package com.example.my_last;
 
+
+import android.util.Log;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
     
-    private static final String BASE_URL = "http://13.124.159.116:8000";
+    private static final String BASE_URL = BuildConfig.BASE_URL;
 
     private static Retrofit retrofit = null;
-
     public static Retrofit getClient() {
+        Log.d("error",BASE_URL);
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
