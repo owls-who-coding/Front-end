78983e5e14480cfaee451eccdc436ecae1cdbb45
joonhee305 <joonhee305@naver.com>
2023-09-10 19:49:37 +0900

    fix:ip

--- app/src/main/java/com/example/my_last/RetrofitClient.java
+++ app/src/main/java/com/example/my_last/RetrofitClient.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClient {
 
     
-    private static final String BASE_URL = "https://18b2-118-222-88-117.jp.ngrok.io";
+    private static final String BASE_URL = "http://13.124.159.116:8000/";
 
     private static Retrofit retrofit = null;
 
